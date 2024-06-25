dfb2aa9655abdac2fd28ec8d5e42482fec5e2e43
lego7set <84934297+lego7set@users.noreply.github.com>
2024-06-25 13:32:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import Ctch from "catch-functions";
+
 export default class Runtime {
   constructor(sandbox) {
     this.sandbox = sandbox;
   }
 
   sandboxRun(code) {
+    this.sandbox.contentWindow.ctch = Ctch;
     this.sandbox.contentWindow.eval(code); // literally just eval the code.
     // will add more sandbox things later, including the catch-functions
   }
